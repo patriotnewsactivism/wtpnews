afd5d2bad673d37865e189837ca63eae212e2097
patriotnewsactivism <patriotnewsactivism@gmail.com>
2025-09-13 14:18:28 -0500

    fix: replace LFS pointer with real postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,7 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:6bbbfd323867a78af8d7ee348a179835202fc7a71f4c2b3e3f34de6f5f895c76
-size 81
+/** @type {import('postcss-load-config').Config} */
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {}
+  }
+};
